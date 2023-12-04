64ab53f7c673960abdd1c11667cddd2afc98aa13
gavril0va1707 <gavril0va1707@yandex.ru>
2023-12-04 10:33:43 +0300

    task064

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+
+ Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Результат: " + GetNumbersDescending(n));
+    }
+
+    static string GetNumbersDescending(int number)
+    {
+        if (number == 1)
+        {
+            return "1";
+        }
+        else
+        {
+            return number + ", " + GetNumbersDescending(number - 1);
+        }
+    }
+}
